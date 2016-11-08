dc4990a0469f14213faf8cccb8585291a65e18f5
Asawari Vaidya <asawari.vaidya@opusconsulting.com>
2016-11-08 18:09:48 +0530

    Adding DirectDebitSampleServlet.java in samples

---
+++ samples/src/main/java/com/paysafe/websample/DirectDebitSampleServlet.java
@@ -0,0 +1,131 @@
+/*
+ * Copyright (c) 2016 Paysafe
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package com.paysafe.websample;
+
+import java.io.IOException;
+
+import java.io.PrintWriter;
+
+import javax.servlet.http.*;
+import javax.servlet.*;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.paysafe.Environment;
+import com.paysafe.PaysafeApiClient;
+import com.paysafe.common.PayMethod;
+import com.paysafe.common.PaysafeException;
+import com.paysafe.customervault.AccountType;
+import com.paysafe.directdebit.Purchases;
+
+public class DirectDebitSampleServlet extends PaysafeServletBase {
+
+  /**
+   *
+   */
+  private static final long serialVersionUID = 1L;
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+    request.setAttribute("isPost", "false");
+    request.setAttribute("currency", this.currencyCode);
+    
+    RequestDispatcher view = request.getRequestDispatcher("directDebitSample.jsp");
+    view.forward(request, response);
+
+   }
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+	// Initiate a new instance of the DirectDebit Purchase Service
+   //Initialize the PaysafeApiClient 
+   
+	PrintWriter out = response.getWriter();
+    boolean wasSuccessfull = false;
+    Purchases purchasesResponse=null;
+    PaysafeApiClient apiClient = new PaysafeApiClient(this.apiKey, this.apiPassword, Environment.TEST, this.accountNumber);
+    	try {
+    	
+    	Purchases purchases=
+    			Purchases.builder()
+    		
+       		 .merchantRefNum(request.getParameter("merchantRefNum"))
+       		 .amount(Integer.parseInt(request.getParameter("amount")))
+       		 .ach()
+       		 	.accountHolderName(request.getParameter("accountHolderName"))
+       		 	.accountType(AccountType.CHECKING)
+       		 	.accountNumber(request.getParameter("accountNumber"))
+       		 	.routingNumber(request.getParameter("routingNumber"))
+       		 	.payMethod(PayMethod.WEB)
+       		 .done()
+       		 .customerIp(request.getParameter("customerIp"))
+       		 .profile()
+       		 	.firstName(request.getParameter("firstName"))
+       		 	.lastName(request.getParameter("lastName"))
+       		 	.email(request.getParameter("email"))
+       		 .done()
+       		 .billingDetails()
+       		 	.street(request.getParameter("street"))
+       		 	.city(request.getParameter("city"))
+       		 	.state(request.getParameter("state"))
+       		 	.country(request.getParameter("country"))
+       		 	.zip(request.getParameter("zip"))
+       		 	.phone(request.getParameter("phone"))
+       		 .done()
+       		 .build();
+    	
+    	
+    	purchasesResponse = apiClient.directDebitService().submitPurchase(purchases);
+    	final GsonBuilder gsonBuilder = new GsonBuilder();
+        gsonBuilder.excludeFieldsWithoutExposeAnnotation();
+        final Gson gson = gsonBuilder.create();
+        
+        
+        out.println("response"+gson.toJson(purchasesResponse));
+    
+    	
+		wasSuccessfull = true;
+    				
+		} catch (PaysafeException e) {
+			// TODO Auto-generated catch block
+			request.setAttribute("error", e.getMessage());
+			out.println(e.getMessage());
+		}
+    	
+   	
+    	request.setAttribute("purchases", purchasesResponse);
+
+        // Create a new diredtdebit request
+        request.setAttribute("isPost", "true");
+        request.setAttribute("currency", this.currencyCode);
+        
+        if (wasSuccessfull) {
+            RequestDispatcher view = request.getRequestDispatcher("status.jsp");
+            view.forward(request, response);
+          } else {
+            RequestDispatcher view = request.getRequestDispatcher("directDebitACHPurchase.jsp");
+            view.forward(request, response);
+          }
+               
+  }
+
+}
--- samples/src/main/webapp/directDebitSample.jsp
+++ samples/src/main/webapp/directDebitSample.jsp
@@ -1,24 +1,104 @@
+<%@page import="com.paysafe.websample.CustomerVaultACHBank"%>
 <html>
 <head></head>
 <body>
 <div>
-<form method="post" action="direct-debit-sample-payment-token">
+<form method="post" action="direct-debit-sample">
 
+<fieldset>
+            <legend>Direct Debit Purchase</legend>
 <div>
-	<input type="radio" name="account" value="ach" checked> ACH<br>
-  	<input type="radio" name="account" value="eft">EFT<br>
-  	<input type="radio" name="account" value="sepa">SEPA<br> 
-  	<input type="radio" name="account" value="bacs">BACS<br>
+	
+	<div>
+		<label>MerchantRefNo</label>
+		<input type="text" name="merchantRefNum" value="<%=java.util.UUID.randomUUID().toString()%>"><br>
+	</div>
+	<div>
+		<label>Amount</label>
+		<input type="text" name="amount" value="10098"><br>
+	</div>
+	<div>
+		<fieldset>
+		<legend>ACH</legend>
+		<div>
+			<label>Account Holder Name</label>
+			<input type="text" name="accountHolderName" value="XYZ Company"><br>
+		</div>
+		<!-- <div>
+			<label>Account Type</label>
+			<input type="text" name="accountType"><br>	
+		</div> -->
+		<div>
+			<label>Account Number</label>
+			<input type="text" name="accountNumber" value="<%= new CustomerVaultACHBank().sixDigitRandomNumber()%>"><br>	
+		</div>
+		<div>
+			<label>Routing Number</label>
+			<input type="text" name="routingNumber" value="211589828"><br>	
+		</div>
+		</fieldset>
+	</div>
+	<div>
+		<label>Customer Ip</label>
+		<input type="text" name="customerIp" value="192.0.126.111"><br>
+	</div>
+	
+	<div>
+		<fieldset>
+		<legend>Profile</legend>
+		
+		<div>
+			<label>First Name</label>
+			<input type="text" name="firstName" value="Joe"><br>	
+		</div>
+		<div>
+			<label>Last Name</label>
+			<input type="text" name="lastName" value="Smith"><br>	
+		</div>
+		<div>
+			<label>Email</label>
+			<input type="text" name="email" value="Joe.Smith@hotmail.com"><br>	
+		</div>
+		</fieldset>
+	</div>
+	<div>
+		<fieldset>
+		<legend>Billing Address</legend>
+		
+		<div>
+			<label>Street</label><input type="text" name="street" value="100 Queen Street West"><br>
+		</div>
+		<div>
+			<label>City</label>
+			<input type="text" name="city" value="Los Angeles"><br>
+		</div>
+		<div>
+			<label>State</label>
+			<input type="text" name="state" value="CA"><br>
+		</div>
+		<div>
+			<label>Country</label>
+			<input type="text" name="country" value="US"><br>	
+		</div>
+		<div>
+			<label>Zip</label>
+			<input type="text" name="zip" value="90210"><br>
+		</div>
+		<div>
+			<label>Phone</label>
+			<input type="text" name="phone" value="3102649010"><br>
+		</div>
+		</fieldset>
+	</div>
+	
+	
 </div>
 <div>
-  	
-  	<label>MerchantRefNo</label><input type="text" name="merchantRefNum" value="<%=java.util.UUID.randomUUID().toString()%>"><br>
-  	<label>Amount</label><input type="text" name="amount" value="10098"><br>
-  	<label>Payment Token</label><input type="text" name="paymentToken"><br>
+	<input type="submit" value="Submit">
 </div>
   	
-  	<input type="submit" value="Submit">
   	
+</fieldset>	
 </form>
 </div>
 
